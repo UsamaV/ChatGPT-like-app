3799442ddf0deca2ec2924303c11e1f9131e3ff4
Usama Virk <usamanasir788@gmail.com>
2023-02-12 21:30:34 +0500

    uploading files

---
+++ client/script.js
@@ -0,0 +1,17 @@
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
+
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
+
+function loader(element) {
+  element.textContent = ''
+
+  loadInterval = setInterval(() => {
+      element.textContent += '.';
+
+      if (element.textContent === '....') {
+          element.textContent = '';
+      }
+  }, 300);
+}
